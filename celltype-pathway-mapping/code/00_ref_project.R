c3ad4deffb6763fe30f6fe3f962aff7fe8977f17
caleblareau <caleb.lareau@gmail.com>
2025-04-29 15:59:10 -0400

    cell type / pathways

---
+++ celltype-pathway-mapping/code/00_ref_project.R
@@ -0,0 +1,62 @@
+library(Seurat)
+library(sctransform)
+library(dplyr)
+library(BuenColors)
+library(Matrix)
+library(data.table)
+library(cowplot)
+library(SeuratDisk)
+library(viridis)
+library(annotables)
+
+# import reference
+ref_path <- "~/Dropbox/main_papers/pearson/pearson_large_data_files/input/pbmc/pbmc_multimodal.h5seurat"
+
+options(future.globals.maxSize = 4000 * 1024^2)
+reference <- LoadH5Seurat(ref_path)
+
+# Load hit genes
+ebv_hits <- (fread("../data/ebv_hits.txt", header = FALSE)[[1]])
+ss <- (rownames(reference@assays$SCT))[(rownames(reference@assays$SCT) %in% ebv_hits) & !grepl("HLA", rownames(reference@assays$SCT))]
+ss <- ss[ss != "RPS18"]
+reference <- AddModuleScore(reference, features = list(ebv_hits, ss), name = "EBV_hits")
+
+pu <- FeaturePlot(reference, c("EBV_hits1"), raster = FALSE, reduction = "wnn.umap", max.cutoff = "q95", min.cutoff = "q05") &
+  scale_color_viridis() & theme_void() & theme(legend.position = "none") 
+cowplot::ggsave2(pu, file = "../plots/umap_ebv_score.png", width = 6, height = 6.1, dpi = 400)
+
+pu2 <- FeaturePlot(reference, c("EBV_hits1"), raster = FALSE, reduction = "wnn.umap", max.cutoff = "q95", min.cutoff = "q05") &
+  scale_color_gradientn(colors = c("lightgrey", jdb_palette("solar_rojos")[c(2:9)])) & 
+  theme_void() & theme(legend.position = "none") 
+cowplot::ggsave2(pu2, file = "../plots/umap_ebv_score_reds.png", width = 6, height = 6.1, dpi = 400)
+
+
+
+px <- ggplot(reference@meta.data, aes(x = celltype.l2, y = EBV_hits1)) +
+  geom_violin(aes(reorder(celltype.l2, EBV_hits1, mean, decreasing = TRUE))) + 
+  geom_boxplot(outlier.shape = NA, fill = NA, color = "firebrick", width = 0.5) +
+  pretty_plot(fontsize = 8) + labs(x = "Annotated cell type", y = "EBV hit module score") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  L_border()
+cowplot::ggsave2(px, file = "../plots/box_ebv_azimuth_L2.pdf", width = 5, height = 2.5)
+
+
+pb <- ggplot(reference@meta.data %>% filter(!(celltype.l1 %in% c("other", "other T"))), aes(x = celltype.l1, y = EBV_hits1)) +
+  geom_violin() + 
+  geom_boxplot(outlier.shape = NA, fill = NA, color = "firebrick", width = 0.5) +
+  pretty_plot(fontsize = 8) + labs(x = "Annotated cell type", y = "EBV hit module score") +
+  scale_x_discrete(limits=rev) +
+  L_border() + coord_flip()
+cowplot::ggsave2(pb, file = "../plots/box_ebv_module.pdf", width = 1.8, height = 2.2)
+
+pu2 <- DimPlot(reference, reduction = "wnn.umap", group.by = "celltype.l2", raster = FALSE) +
+  scale_color_manual(values = (c(jdb_palette("corona"), "salmon"))) +
+  theme_void() + theme(legend.position = "none")
+cowplot::ggsave2(pu2, file = "../plots/seurat_baseline.png", width = 6, height = 6.1, dpi = 400)
+
+
+pl1 <- DimPlot(reference, reduction = "wnn.umap", group.by = "celltype.l1", raster = FALSE) +
+  scale_color_manual(values = (c(jdb_palette("corona")[c(1:3,6,4,5)],"lightgrey", "lightgrey"))) +
+  theme_void() + theme(legend.position = "none")
+
+cowplot::ggsave2(pl1, file = "../plots/seurat_baseline_L1.png", width = 6, height = 6.1, dpi = 400)
---
+++ celltype-pathway-mapping/code/01_genesetanalysis.R
@@ -0,0 +1,122 @@
+library(BuenColors)
+library(clusterProfiler)
+library(ReactomePA)
+library(clusterProfiler)
+library(dplyr)
+library(org.Hs.eg.db)
+library(annotables)
+library(viridis)
+
+# Function to compute fold enrichments
+calc_fold_enrichments <- function(a_df){
+  new_df <- a_df %>% dplyr::select(GeneRatio, BgRatio) %>% 
+    tidyr::separate(GeneRatio, into = c("GA","GB"), sep = "/", remove = TRUE, convert = TRUE) %>% 
+    tidyr::separate(BgRatio, into = c("BA","BB"), sep = "/", remove = TRUE, convert = TRUE) 
+  
+  new_df <- new_df %>% mutate(gene_ratio = GA/GB, bg_ratio = BA/BB, fold_enrichment= gene_ratio/bg_ratio)
+  a_df <- cbind(a_df,new_df)
+  return(a_df)
+}
+
+# Options for cluster profiler
+options(clusterProfiler.download.method = 'curl')
+options(download.file.extra = '-k')
+options(clusterProfiler.download.method = 'wget')
+options(download.file.extra = '--no-check-certificate')
+
+# Import enriched hits
+non_chr6_hits <- readLines("../data/EBV_0.0018_Ch6rem.txt")
+non_hla_hits <- readLines("../data/EBV_0.0018_Ch6inc_HLArem.txt")
+all_hits <- readLines("../data/ebv_hits.txt")
+
+# Define universe to do chromosome 6 enrichment
+universe_df <- grch38 %>% filter(biotype == "protein_coding" & chr %in% c(1:22, "X"))
+universe_df_noHLA <- universe_df[!grepl("HLA", universe_df$symbol),]
+universe_df_no6 <- universe_df %>% filter(chr != 6)
+
+# Run enrichment now
+map_genes <- function(vec){
+  bitr(vec, fromType="SYMBOL",
+       toType = c("ENSEMBL", "ENTREZID"), 
+       OrgDb="org.Hs.eg.db")
+}
+enriched_genes_no6 <- map_genes(non_chr6_hits)
+enriched_genes_noHLA <- map_genes(non_hla_hits)
+enriched_genes <- map_genes(all_hits)
+
+# Define background
+background_no6 <- map_genes(universe_df_no6$symbol)
+background_noHLA <- map_genes(universe_df_noHLA$symbol)
+background_allgenes <- map_genes(universe_df$symbol)
+
+# Do enrichment
+goBP_enrich_no6 <- enrichGO(gene = enriched_genes_no6$ENSEMBL,
+                            OrgDb         = org.Hs.eg.db,
+                            keyType       = 'ENSEMBL',
+                            ont           = "BP",
+                            pAdjustMethod = "BH",
+                            universe = background_no6$ENSEMBL, 
+                            qvalueCutoff  = 0.05) %>%  simplify(. , cutoff = 0.7) %>% as.data.frame() %>% calc_fold_enrichments()
+
+goBP_enrich_noHLA <- enrichGO(gene = enriched_genes_noHLA$ENSEMBL,
+                              OrgDb         = org.Hs.eg.db,
+                              keyType       = 'ENSEMBL',
+                              ont           = "BP",
+                              pAdjustMethod = "BH",
+                              universe = background_noHLA$ENSEMBL, 
+                              qvalueCutoff  = 0.05) %>%  simplify(. , cutoff = 0.7) %>% as.data.frame() %>% calc_fold_enrichments()
+
+goBP_enrich_all <- enrichGO(gene = enriched_genes$ENSEMBL,
+                            OrgDb         = org.Hs.eg.db,
+                            keyType       = 'ENSEMBL',
+                            ont           = "BP",
+                            pAdjustMethod = "BH",
+                            universe = background_allgenes$ENSEMBL, 
+                            qvalueCutoff  = 0.05)  %>%  simplify(. , cutoff = 0.7) %>% as.data.frame() %>% calc_fold_enrichments()
+
+# Function to make enrichment plot automatically
+make_enrichment_plot <- function(in_df){
+  
+  in_df %>% 
+    arrange((zScore)) %>%
+    mutate(rank = 1:n()) %>% 
+    tail(5) -> plot_df 
+  
+  plot_df %>%
+    ggplot(aes(x = rank, y = zScore, fill = -log10(qvalue))) + 
+    geom_bar(stat = "identity", color = "black") +
+    coord_flip() +
+    scale_y_continuous(expand = c(0,0)) + 
+    pretty_plot(fontsize = 8) + L_border() +
+    scale_fill_gradientn(limits = c(0, max(-log10(in_df$qvalue))), oob = scales::squish, colors = jdb_palette("solar_rojos")) +
+    theme(legend.position = "bottom") + labs( x= "", y = "enrichment z-score", fill = "")
+}
+
+p1 <- make_enrichment_plot(goBP_enrich_all)
+p2 <- make_enrichment_plot(goBP_enrich_noHLA)
+p3 <- make_enrichment_plot(goBP_enrich_no6)
+cowplot::ggsave2(
+  cowplot::plot_grid(p1, p2, p3, nrow = 1), 
+  width = 6, height = 1.8, file = "../plots/enrich_BP.pdf"
+)
+
+head(goBP_enrich_noHLA[,!(colnames(goBP_enrich_noHLA) %in% c("geneID"))])
+head(goBP_enrich_all[,!(colnames(goBP_enrich_all) %in% c("geneID"))])
+head(goBP_enrich_no6[,!(colnames(goBP_enrich_no6) %in% c("geneID"))])
+
+library(ggrepel)
+p0 <- ggplot(goBP_enrich_all, aes(x = FoldEnrichment, y = -log10(qvalue), label = Description)) +
+  geom_point(size = 0.5) + geom_text_repel(max.overlaps = 5,  size = 1) +
+  pretty_plot(fontsize = 8) + L_border()
+
+
+kegg_enrich <- enrichKEGG(gene = enriched_genes$ENTREZID,
+                            pAdjustMethod = "BH",
+                            universe = background_allgenes$ENTREZID, 
+                            qvalueCutoff  = 0.05) %>% as.data.frame()
+
+p1 <- ggplot(kegg_enrich, aes(x = FoldEnrichment, y = -log10(qvalue), label = Description)) +
+  geom_point(size = 0.5) + geom_text_repel(max.overlaps = 5, size = 1) +
+  pretty_plot(fontsize = 8) + L_border()
+cowplot::ggsave2(p0, file = "../plots/go_bp_volcano.pdf", width = 3.75, height = 2.5)
+cowplot::ggsave2(p1, file = "../plots/kegg_volcano.pdf", width = 3.75, height = 2.5)
