c8b0471a6aeba2fb5749ea0e777d475f7ae3f074
Sherry Nyeo <sherrynyeo4009@gmail.com>
2025-08-29 18:38:31 -0400

    Updated AoU T1K pipeline

--- all-of-us-notebooks/HLA_haplotype_construction/compute_hla_regions.R
+++ all-of-us-notebooks/HLA_haplotype_construction/compute_hla_regions.R
@@ -1,25 +1,25 @@
-library(rtracklayer) 
+library(rtracklayer)
 library(dplyr)
-library(stringr) 
+library(stringr)
 
-## import gtf 
+## import gtf
 # This file can be obtained here: https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/
 gtf <- import("/data1/lareauc/references/hg38_variant_calling/hg38.ncbiRefSeq.gtf")
-# subset to chr6 
+# subset to chr6
 gtf2 <- gtf[seqnames(gtf) == "chr6"]
-# Remove HLA genes 
+# Remove HLA genes
 catch <- gtf2[str_detect(gtf2$gene_name,"HLA")]
-# subset to transcripts 
+# subset to transcripts
 catch2 <- catch[catch$type == "transcript"]
 
-## Include TAP1/2 and MICA/B 
+## Include TAP1/2 and MICA/B
 catch <- gtf2[str_detect(gtf2$gene_name,"^TAP[1|2]$")]
 toadd <- catch[catch$type == "transcript"]
 
 catch <- gtf2[str_detect(gtf2$gene_name,"^MIC[A|B]$")]
 toadd2 <- catch[catch$type == "transcript"]
 
-joint <- c(catch2,toadd,toadd2) 
+joint <- c(catch2,toadd,toadd2)
 
 # simplify regions to only 100 kB windows for all HLA genes
 try1 <- reduce(joint, min.gapwidth=100000)
@@ -28,8 +28,9 @@ try1 <- reduce(joint, min.gapwidth=100000)
 width(try1) %>% sum() #1549980 bp total
 
 #chr6:28510120-33480577
-#4970457bps used, 4970457 / 1321933 === 3.2 decrease in size so that saves a lot 
+#4970457bps used, 4970457 / 1321933 === 3.2 decrease in size so that saves a lot
 
 out <- try1 %>% as.data.frame() %>% mutate(out = sprintf("%s:%s-%s",seqnames,start,end)) %>% pull(out)
 
-writeLines(out,"small_hla_regions_nounmapped.interval")
+# this file is further processed in the Jupyter 01_Run_T1K notebook
+writeLines(out,"chr6_simple_regions_v2.txt")
